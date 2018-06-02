06390e2f44616f38a61ef6200ede32e32e4d8116
Juan Leiro <juan.leiro@gmail.com>
2018-06-02 13:29:28 -0300

    commit inicial

---
+++ src/main/java/br/com/se/oxp/api/OxpApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.se.oxp.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxpApiApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/se/oxp/api/OxpApiApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.se.oxp.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OxpApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
